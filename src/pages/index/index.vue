cb66247bcd4e583be90123077f76eb46901d3906
Ganduo <jianzhitianya63@163.com>
2025-06-22 16:10:56 +0800

    fix: resolve login issue

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -11,8 +11,6 @@
 </route>
 
 <script lang="ts" setup>
-import PLATFORM from '@/utils/platform'
-
 defineOptions({
   name: 'Home',
 })
@@ -43,6 +41,11 @@ const author = ref('菲鸽')
 const description = ref(
   'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite5 + UnoCss + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
 )
+const bannerList = [
+  '/static/images/banner1.jpg',
+  '/static/images/banner2.jpg',
+  '/static/images/banner3.jpg',
+]
 // 测试 uni API 自动引入
 onLoad(() => {
   console.log('项目作者:', author.value)
@@ -52,31 +55,13 @@ console.log('index')
 </script>
 
 <template>
-  <view class="bg-white px-4 pt-2" :style="{ marginTop: `${safeAreaInsets?.top}px` }">
-    <view class="mt-12">
-      <image src="/static/logo.svg" alt="" class="mx-auto block h-28 w-28" />
-    </view>
-    <view class="mt-4 text-center text-4xl text-[#d14328]">
-      unibest
-    </view>
-    <view class="mb-8 mt-2 text-center text-2xl">
-      最好用的 uniapp 开发模板
-    </view>
-
-    <view class="m-auto mb-2 max-w-100 text-justify indent text-4">
-      {{ description }}
-    </view>
-    <view class="mt-8 text-center">
-      当前平台是：
-      <text class="text-green-500">
-        {{ PLATFORM.platform }}
-      </text>
-    </view>
-    <view class="mt-4 text-center">
-      模板分支是：
-      <text class="text-green-500">
-        base
-      </text>
+  <view>
+    <view>
+      <swiper class="h-400rpx">
+        <swiper-item v-for="(item, index) in bannerList" :key="index">
+          <image :src="item" mode="aspectFill" class="wh-full" />
+        </swiper-item>
+      </swiper>
     </view>
   </view>
 </template>
